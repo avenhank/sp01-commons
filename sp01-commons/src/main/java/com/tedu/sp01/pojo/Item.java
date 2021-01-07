12e4d0d138cf817d0a9c98bb5aaf0c583ac846e7
HAN <HAN@kaka>
2021-01-07 11:43:53 +0800

    sp01-commons初始化

---
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -0,0 +1,18 @@
+package com.tedu.sp01.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+	//编码
+	private Integer id;
+	//名称
+	private String name;
+	private Integer number;
+	//异常修改
+	//1111
+}
